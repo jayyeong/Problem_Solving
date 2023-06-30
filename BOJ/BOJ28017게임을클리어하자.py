4aa2ea051c7277d2ce9bde07f7b861b9053a8c83
jayyeong <48705640+jayyeong@users.noreply.github.com>
2023-07-01 03:13:03 +0900

    solved

--- BOJ/BOJ28017게임을클리어하자.py
+++ BOJ/BOJ28017게임을클리어하자.py
@@ -1,26 +1,24 @@
 import sys
 
-INF = int(1e9)
+#INF = int(1e9)
 N, M = map(int, input().split())
 arr = []
 
 for _ in range(N):
     arr.append([int(x) for x in sys.stdin.readline().split()])
 
-box = [[0] * M for _ in range(N)]
-
-box[0] = arr[0]
-
 for i in range(N - 1):
 
     for j in range(M):
 
-        temp = INF
+        #temp = INF
 
-        for k in range(M):
-            if box[i][k] <= temp and j != k:
-                temp = box[i][k]
+        #for k in range(M):
+        #    if arr[i][k] <= temp and j != k:
+        #        temp = arr[i][k]
 
-        box[i + 1][j] = arr[i + 1][j] + temp
+        temp = min(arr[i][:j] + arr[i][j + 1:])
 
-print(min(box[N - 1]))
+        arr[i + 1][j] += temp
+
+print(min(arr[N - 1]))
