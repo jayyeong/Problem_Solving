464a4dfdde5e5ee16ef8e27ba7c9ddae2be28eb8
jayyeong <48705640+jayyeong@users.noreply.github.com>
2024-03-28 14:40:10 +0900

    solved

---
+++ BOJ/BOJ25206너의평점은.py
@@ -0,0 +1,16 @@
+import sys
+
+gradeTable = {'A+':4.5, 'A0':4.0, 'B+':3.5, 'B0':3.0, 'C+':2.5,
+       'C0':2.0,'D+':1.5, 'D0':1.0, 'F':0.0}
+
+answer = 0.0
+totalCredit = 0.0
+
+for _ in range(20):
+    subject, credit, grade  = sys.stdin.readline().rstrip().split()
+
+    if grade != 'P':
+        totalCredit += float(credit)
+        answer += float(credit) * gradeTable[grade]
+
+print("{:.6f}".format(answer/totalCredit))
--- BOJBronze.py
+++ BOJBronze.py
@@ -1,16 +1,9 @@
-N, M = map(int, input().split())
+word = list(input())
 
-numList = [int(x) for x in range(1, N + 1)]
+w = len(word)
 
-for _ in range(M):
-    i, j = map(int, input().split())
-
-    i -= 1
-    j -= 1
-
-    for k in range((j - i + 1)//2):
-        tmp = numList[i + k]
-        numList[i + k] = numList[j - k]
-        numList[j - k] = tmp
-
-print(' '.join(map(str, numList)))
+for i in range(w//2):
+    if word[i] != word[w - 1 - i]:
+        print(0)
+        exit()
+print(1)
