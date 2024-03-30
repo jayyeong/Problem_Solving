32fbfc920cd9d350182a231457a13a8e48eb3c49
jayyeong <48705640+jayyeong@users.noreply.github.com>
2024-03-30 15:19:24 +0900

    solved

--- BOJBronze.py
+++ BOJBronze.py
@@ -1,9 +1,3 @@
-word = list(input())
+N = int(input())
 
-w = len(word)
-
-for i in range(w//2):
-    if word[i] != word[w - 1 - i]:
-        print(0)
-        exit()
-print(1)
+print((1 + 2 ** N) ** 2)
